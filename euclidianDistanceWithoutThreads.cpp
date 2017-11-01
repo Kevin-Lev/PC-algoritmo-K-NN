d7f71f831447b2c57aa633fcabb3741227c77ba8
Kevin-Lev <kevinlevrone.r@gmail.com>
2017-11-01 19:11:51 -0200

    Correções no cálculo da distância

--- euclidianDistanceWithoutThreads.cpp
+++ euclidianDistanceWithoutThreads.cpp
@@ -11,15 +11,15 @@ int main () { //Euclidian
   float Pmembers[matrixColumn] = {1, 2, 3, 4, 5},
         Qmembers[matrixColumn] = {0, 3, 2, 7, 4},
         euclidianDistance[matrixRow] = {0, 0, 0, 0, 0};
-        
+
   float aux = 0.0;
 
   for (int row = 0; row < matrixRow; row++) {
-    for (int column = 0; column < matrixColumn; column++) { 
-      aux = abs(pow(Pmembers[row] - Qmembers[column], 2.0));
+    for (int column = 0; column < matrixColumn; column++) {
+      aux = abs(pow(Pmembers[row][column] - Qmembers[][column], 2.0));
       euclidianDistance[row] += aux;
     }
-    
+
     euclidianDistance[row] = sqrt(euclidianDistance[row]);
     cout << euclidianDistance[row] << endl;
     aux = 0;
