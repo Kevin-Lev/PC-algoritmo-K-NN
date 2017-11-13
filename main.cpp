64d711329e680f6dc527afe150d05fc8b69f8632
Kevin-Lev <kevinlevrone.r@gmail.com>
2017-11-13 16:08:18 -0200

    Funções sequencial e concorrente finalizadas

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <time.h>
 
 using namespace std;
 
@@ -30,6 +31,12 @@ struct distanciaClasse {
   float distancia;
 };
 
+struct argsfuncDist {
+  int linhasTest, linhasBase, numThreads, posInicial, posFinal, inicioI, idThread;
+  dadosArquivo *dadosTest;
+  dadosArquivo *dadosBase;
+};
+
 void printaVetor(dadosArquivo vetor[], int tamVetor){
   for(int i=0;i<tamVetor;i++){
     for(int j=0;j<vetor[i].linhasArquivo.size();j++){
@@ -40,7 +47,7 @@ void printaVetor(dadosArquivo vetor[], int tamVetor){
 }
 
 void insereVetor(int tamLinhas, dadosArquivo *vetorDados, string nomeArquivo){
-  int j = 0, k = 0, cont = 0;
+  int j = 0, k = 0;
   float numchars;
   string linhaArquivo, aux;
   fstream arquivo;
@@ -48,7 +55,7 @@ void insereVetor(int tamLinhas, dadosArquivo *vetorDados, string nomeArquivo){
   arquivo.open(nomeArquivo);
 
   while(getline(arquivo, linhaArquivo)){
-    k = linhaArquivo.find_first_not_of("0123456789.,");
+    k = linhaArquivo.find_first_not_of("0123456789.,-");
     for(int l=k;l<=linhaArquivo.size();l++){
       aux += linhaArquivo[l];
     }
@@ -69,9 +76,51 @@ void insereVetor(int tamLinhas, dadosArquivo *vetorDados, string nomeArquivo){
   }
 }
 
-vector<distanciaClasse> vetorDistancias;
-vector<float> vetMenoresDist;
 
+vector<distanciaClasse> vetorDistancias;
+distanciaClasse *vetorAux;
+pthread_barrier_t barreira;
+
+
+void *threadDistancia(void *arg){
+  pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+  float menorDistancia = 100000;
+  distanciaClasse distClasse;
+  float distancia = 0;
+  string nomeGenero;
+  argsfuncDist *argDist = (argsfuncDist*) arg;
+
+
+  for(int i=0;i<argDist->linhasTest;i++){
+    for(int j=argDist->posInicial;j<=argDist->posFinal;j++){
+      for(int k=0;k<argDist->dadosBase[j].linhasArquivo.size();k++){
+        distancia = distancia + abs(pow((argDist->dadosTest[i].linhasArquivo[k] - argDist->dadosBase[j].linhasArquivo[k]), 2));
+      }
+      distClasse.distancia = sqrt(distancia);
+      distClasse.genero = argDist->dadosBase[j].genero;
+      pthread_mutex_lock(&mut);
+      vetorAux[j] = distClasse;
+      pthread_mutex_unlock(&mut);
+      distancia = 0;
+    }
+
+    pthread_barrier_wait(&barreira);
+    if(argDist->idThread == 0){
+      distanciaClasse distAux;
+      distAux.distancia = 100000;
+      for(int l=0;l<argDist->linhasBase;l++){
+        if(vetorAux[l].distancia < distAux.distancia){
+          distAux.genero = vetorAux[l].genero;
+          distAux.distancia = vetorAux[l].distancia;
+        }
+      }
+      vetorDistancias.push_back(distAux);
+    }
+    distancia = 0;
+    pthread_barrier_wait(&barreira);
+  }
+
+}
 
 vector<distanciaClasse> calculaDistancia(int linhasTest, int linhasBase, dadosArquivo dadosTest[], dadosArquivo dadosBase[]){
   float menorDistancia = 100000;
@@ -84,17 +133,20 @@ vector<distanciaClasse> calculaDistancia(int linhasTest, int linhasBase, dadosAr
       for(int k=0;k<dadosBase[j].linhasArquivo.size();k++){  // dadosTest.linhasArquivo e dadosBase.linhasArquivo tem o mesmo tamanho.
         distancia = distancia + abs(pow((dadosTest[i].linhasArquivo[k] - dadosBase[j].linhasArquivo[k]), 2));
       }
-      distancia = sqrt(distancia);
-      if(distancia < menorDistancia){
-        menorDistancia = distancia;
-        distClasse.genero = dadosBase[j].genero;
-      }
+      distClasse.distancia = sqrt(distancia);
+      distClasse.genero = dadosBase[j].genero;
+      vetorAux[j] = distClasse;
       distancia = 0;
     }
-      distClasse.distancia = menorDistancia;
-      vetorDistancias.push_back(distClasse);
-      menorDistancia = 100000;
-      distancia = 0;
+    distanciaClasse distAux;
+    distAux.distancia = 100000;
+    for(int l=0;l<linhasBase;l++){
+      if(vetorAux[l].distancia < distAux.distancia){
+        distAux.genero = vetorAux[l].genero;
+        distAux.distancia = vetorAux[l].distancia;
+      }
+    }
+      vetorDistancias.push_back(distAux);
   }
   return vetorDistancias;
 }
@@ -103,31 +155,75 @@ vector<distanciaClasse> calculaDistancia(int linhasTest, int linhasBase, dadosAr
 int main(int argc, char* argv[]){
 
   unsigned concurrentThreadsSupported = std::thread::hardware_concurrency();
-  string arquivoTest = argv[1], arquivoBase = argv[2];
-  int k=0, l=0, args[concurrentThreadsSupported], linhasTest = contaLinhas(arquivoTest), linhasBase = contaLinhas(arquivoBase);
-  float aux, distancia = 0;
+  int args[concurrentThreadsSupported], linhasTest = contaLinhas(argv[1]), linhasBase = contaLinhas(argv[2]);
+  int divisaoBase, restoBase, distancia = 0;
+  distanciaClasse auxiliarLocal[linhasBase];
   dadosArquivo dadosTest[linhasTest], dadosBase[linhasBase];
+  argsfuncDist vetorParametros[concurrentThreadsSupported];
   pthread_t thread[concurrentThreadsSupported];
+  pthread_barrier_init(&barreira, NULL, concurrentThreadsSupported);
+  time_t inicio, inicioThreads, fim, fimThreads;
 
-  insereVetor(linhasTest, dadosTest, arquivoTest);
-  insereVetor(linhasBase, dadosBase, arquivoBase);
+
+  insereVetor(linhasTest, dadosTest, argv[1]);
+  insereVetor(linhasBase, dadosBase, argv[2]);
   //printaVetor(dadosTest, linhasTest);
   //printaVetor(dadosBase, linhasBase);
 
-  cout << "Este sistema suporta e irá usar " << concurrentThreadsSupported << " Threads" << endl;
-  cout << "Calculando a distância entre as colunas das matrizes..." << endl << endl;
+  vetorAux = auxiliarLocal;
+
+  cout << endl << "Este sistema suporta e irá usar " << concurrentThreadsSupported << " Threads" << endl << endl;
+  cout << "Nome do arquivo de teste: " << argv[1] << endl << "Nome do arquivo de treino " << argv[2] << endl;
+  cout << "Calculando de forma sequencial a distância entre as colunas das matrizes..." << endl << endl;
+
+  divisaoBase = linhasBase/concurrentThreadsSupported;
+  restoBase = linhasBase%concurrentThreadsSupported;
+
+  inicio = time(NULL);
   vetorDistancias = calculaDistancia(linhasTest, linhasBase, dadosTest, dadosBase);
-  for(int i=0;i<vetorDistancias.size();i++){
-    cout << i << endl << endl;
-    cout << vetorDistancias[i].genero << endl;
-    cout << vetorDistancias[i].distancia << endl << endl;
+  fim = time(NULL);
+
+  cout << "Calculando de forma concorrente a distância entre as colunas das matrizes..." << endl << endl;
+
+
+  for(int i=0;i<concurrentThreadsSupported;i++){
+    vetorParametros[i].linhasTest = linhasTest;
+    vetorParametros[i].linhasBase = linhasBase;
+    vetorParametros[i].numThreads = concurrentThreadsSupported;
+    vetorParametros[i].posInicial = 0;
+    vetorParametros[i].posFinal = divisaoBase - 1;
+    vetorParametros[i].dadosTest = dadosTest;
+    vetorParametros[i].dadosBase = dadosBase;
   }
 
-  /*for(int j=0; j<linhasTest;j++){
-    for(int i=0;i<concurrentThreadsSupported;i++){
-      args[i] = i+1;
-      pthread_create(&thread[i], NULL,calculaDistancia2, NULL);
-      pthread_join(thread[i], NULL);
+  inicioThreads = time(NULL);
+  for(int i=0;i<concurrentThreadsSupported;i++){
+    vetorParametros[i].idThread = i;
+    if(i > 0){
+      if(i == concurrentThreadsSupported-1){
+        vetorParametros[i].posInicial = vetorParametros[i].posInicial + divisaoBase;
+        vetorParametros[i].posFinal = vetorParametros[i].posFinal + divisaoBase + restoBase;
+      }
+      else{
+        vetorParametros[i].posInicial = vetorParametros[i].posInicial + divisaoBase;
+        vetorParametros[i].posFinal = vetorParametros[i].posFinal + divisaoBase;
+      }
     }
-  }*/
+    pthread_create(&thread[i], NULL, threadDistancia, &vetorParametros[i]);
+    if(i == concurrentThreadsSupported-1){
+      vetorParametros[i].idThread = i;
+      break;
+    }
+  }
+
+  for(int i=0;i<concurrentThreadsSupported;i++){
+    pthread_join(thread[i], NULL);
+  }
+  fimThreads = time(NULL);
+
+  //printaVetor(vetorDistancias);
+
+  cout << "tempo gasto para o cálculo de distância sequencial: " << difftime(fim, inicio) << " segundos" << endl;
+  cout << "tempo gasto para o cálculo de distância concorrente: " << difftime(fimThreads, inicioThreads) << " segundos" << endl;
+
 }
